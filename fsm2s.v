5accf371965c395da09dd36a7894ffa622b2c5e8
Seungbeom <31874379+tmdqja1001@users.noreply.github.com>
2023-03-09 20:17:04 +0900

    Create fsm2s.v

---
+++ fsm2s.v
@@ -0,0 +1,35 @@
+module top_module(
+    input clk,
+    input reset,    // Synchronous reset to OFF
+    input j,
+    input k,
+    output out); //  
+
+    parameter OFF=0, ON=1; 
+    reg state, next_state;
+
+    always @(*) begin
+        // State transition logic
+        if(j==0&state==OFF)
+            next_state <= OFF;
+        else if(j==1&state==OFF)
+            next_state <= ON;
+        else if(k==0&state==ON)
+            next_state <= ON;
+        else
+            next_state <= OFF;
+    end
+
+    always @(posedge clk) begin
+        // State flip-flops with synchronous reset
+        if(reset)
+            state <= OFF;
+        else
+            state <= next_state;
+    end
+
+    // Output logic
+    // assign out = (state == ...);
+    assign out = state;
+
+endmodule
