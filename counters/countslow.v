728f9065e1dd53f87cab78c02a57d7b2fa854beb
Seungbeom <31874379+tmdqja1001@users.noreply.github.com>
2023-03-13 18:16:17 +0900

    Create countslow.v

---
+++ counters/countslow.v
@@ -0,0 +1,18 @@
+module top_module (
+    input clk,
+    input slowena,
+    input reset,
+    output [3:0] q);
+    
+    always @(posedge clk) begin
+        if(reset)
+            q <= 4'b0;
+        else if(q==4'd9 & slowena)
+            q <= 4'b0;
+        else if(slowena)
+            q <= q + 1'b1;
+        else
+            q <= q + 1'b0;
+    end
+
+endmodule
